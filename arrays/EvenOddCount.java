8a47d2575fa8376258b137fe95d96d408cfda6e1
tharshithap <sujipas722@gmail.com>
2025-06-19 10:53:31 +0530

    Update EvenOddCount.java

--- arrays/EvenOddCount.java
+++ arrays/EvenOddCount.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size: ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int arr[] = new int[n];
         System.out.println("Enter elements:");
         int even = 0, odd = 0;
         for(int i=0; i<n; i++) 
