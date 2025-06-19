7505d73014cf1098d62f38a2432540cd5d969e53
tharshithap <sujipas722@gmail.com>
2025-06-19 14:02:18 +0530

    Update MissingNumbers.java

--- arrays/MissingNumbers.java
+++ arrays/MissingNumbers.java
@@ -7,16 +7,21 @@ System.out.println("Enter the size:");
         int n = sc.nextInt(); 
         int[] arr = new int[n];
 
-        System.out.println("Enter 99 numbers from 1 to 100 (one number is missing):");
-        int sum = 0;
+        System.out.println("Enter 99 numbers from 1 to 100 (one number is missing):")
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            sum += arr[i];
+           
         }
 
-          int total = 100 * 101 / 2;
-
-        System.out.println("Missing number: " + (total - sum));
+       int j=0;
+        System.out.println("Missing numbers b/w 1 to 100 in the array:");
+        for(int i=1;i<n; i++){
+            
+            if(j<arr.length &&arr[j]==i)
+            {
+                j++;
     }
+            else{
+                System.out.print(i+ "");
 }
