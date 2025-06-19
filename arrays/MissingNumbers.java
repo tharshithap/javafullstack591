9f4c0d8bcb2b0fa1f73eeafc7c443c393574f6dd
tharshithap <sujipas722@gmail.com>
2025-06-19 11:38:02 +0530

    Update MissingNumbers.java

--- arrays/MissingNumbers.java
+++ arrays/MissingNumbers.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
- class MissingNumber {
+
+class MissingNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 99; 
-        int arr[]= new int[n];
-        System.out.println("Enter 99 numbers from 1 to 100 (one missing):");
+System.out.println("Enter the size:");
+        int n = sc.nextInt(); 
+        int[] arr = new int[n];
+
+        System.out.println("Enter 99 numbers from 1 to 100 (one number is missing):");
         int sum = 0;
-        for(int i=0; i<n; i++) {
+
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
-        int total = 100*101/2;
+
+          int total = 100 * 101 / 2;
+
         System.out.println("Missing number: " + (total - sum));
     }
 }
