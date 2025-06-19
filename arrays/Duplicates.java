c24a040a534ddeaa8d47a5d3f1a0e9ff6b2252e1
tharshithap <sujipas722@gmail.com>
2025-06-19 12:00:35 +0530

    Update Duplicates.java

--- arrays/Duplicates.java
+++ arrays/Duplicates.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
- class Duplicates {
-    public static void main(String[] args) 
-{
+
+class Duplicates {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size: ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int arr[] = new int[n];       
         System.out.println("Enter elements:");
-        for(int i=0; i<n; i++) 
-{
-arr[i] = sc.nextInt();
-}
+        for(int i = 0; i < n; i++)
+ {
+            arr[i] = sc.nextInt();
+        }      
         System.out.print("Duplicates: ");
-        boolean found = false;
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(arr[i]==arr[j]) {
-                    System.out.print(arr[i] + " ");
-                    found = true;
+        int count = 0;
+        for(int i = 0; i < n; i++) 
+{            for(int j = i + 1; j < n; j++)
+ {                if(arr[i] == arr[j])
+ {                  int alreadyPrinted = 0;
+                    for(int k = 0; k < i; k++)
+ {
+                        if(arr[k] == arr[i]) 
+{
+                            alreadyPrinted = 1;
+                            break;
+                        }
+                    }
+                    if(alreadyPrinted == 0) {
+                        System.out.print(arr[i] + " ");
+                        count++;
+                    }
                     break;
                 }
             }
         }
-        if(!found) System.out.print("None");
+
+      
+        if(count == 0) {
+            System.out.print("None");
+        }
     }
 }
