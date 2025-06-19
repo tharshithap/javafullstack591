ab63947efc77c855382cccba988e7db83cdf2872
tharshithap <sujipas722@gmail.com>
2025-06-19 11:36:10 +0530

    Update SecondLargest.java

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -1,24 +1,47 @@
 import java.util.Scanner;
- class SecondLargest {
+
+class SecondLargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+      
         System.out.print("Enter size: ");
         int n = sc.nextInt();
+
+       
+        if(n < 2) {
+            System.out.println("Array should have at least two elements.");
+            return;
+        }
+
         int arr[] = new int[n];
+
+      
         System.out.println("Enter elements:");
-        for(int i=0; i<n; i++) 
-{
-arr[i] = sc.nextInt();
-}
-        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
-        for(int num : arr) {
-            if(num > first) {
+        for(int i = 0; i < n; i++)
+         {
+            arr[i] = sc.nextInt();
+        }
+
+           int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
+      
+        for(int i = 0; i < n; i++)
+         {
+            if(arr[i] > first) 
+            {
                 second = first;
-                first = num;
-            } else if(num > second && num != first) {
-                second = num;
+                first = arr[i];
+            } else if(arr[i] > second && arr[i] != first) {
+                second = arr[i];
             }
         }
-        System.out.println("Second largest: " + second);
+
+        
+        if(second == Integer.MIN_VALUE) {
+            System.out.println("No second largest element (all elements may be equal).");
+        } else {
+            System.out.println("Second largest: " + second);
+        }
     }
 }
