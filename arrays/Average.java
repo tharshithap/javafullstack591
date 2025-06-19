d1d2eae2251552615f555d46ade114930b38455d
tharshithap <sujipas722@gmail.com>
2025-06-19 10:45:25 +0530

    Update Average.java

--- arrays/Average.java
+++ arrays/Average.java
@@ -4,7 +4,7 @@ class Average {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size: ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int arr[] = new int[n];
         int sum = 0;
         System.out.println("Enter elements:");
         for(int i=0; i<n; i++) {
