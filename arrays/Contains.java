ff9b9612d6ca9314a48ea5524bab803312cfb506
tharshithap <sujipas722@gmail.com>
2025-06-19 10:47:24 +0530

    Update Contains.java

--- arrays/Contains.java
+++ arrays/Contains.java
@@ -5,7 +5,7 @@ class Contains {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size: ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int arr[]= new int[n];
         System.out.println("Enter elements:");
         for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
