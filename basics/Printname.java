d95fe018a842b54d8b1312373157f1dd07fbdf51
tharshithap <sujipas722@gmail.com>
2025-06-20 14:35:26 +0530

    Delete basics directory

--- basics/Printname.java
+++
@@ -1,6 +0,0 @@
-class Printname{
-public static void main(String[] args)
-{
-System.out.println("Tharshitha");
-}
-}
--- basics/Printnamefunction.java
+++
@@ -1,8 +0,0 @@
-class Printnamefunction{
- static void printMyName(){
-System.out.println("Tharshitha");
-}
-public static void main(String[] args){
- printMyName();
-}
-}
