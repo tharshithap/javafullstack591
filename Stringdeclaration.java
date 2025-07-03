2e49b9637cfc8fb99eec00836be402508ba833d0
tharshithap <sujipas722@gmail.com>
2025-07-03 21:33:07 +0530

    Create Stringdeclaration.java

---
+++ Stringdeclaration.java
@@ -0,0 +1,9 @@
+class Stringdeclaration {
+static public void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = new String("World");
+        System.out.println(str1);
+        System.out.println(str2);
+}
+}
+
